9a5571f98883906fd4f3e8899c1d3ce69a1cac1e
Andrea Bisognin <andrea.bisognin@gmail.com>
2016-11-16 11:52:22 +0100

    added setup script

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='pdfmagick',
+      version='1.0',
+      description='pdftk wrapper',
+      author='Andrea Bisognin',
+      author_email='andrea.bisognin@gmail.com',
+      url='https://github.com/bisio/pdfmagick',
+      scripts=['pdfmagick']
+      )
